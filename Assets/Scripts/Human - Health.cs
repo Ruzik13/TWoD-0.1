c2e85a62a43b18d8e225bbb8ae8fd2e5ee2ee94f
ArfaAqil <arfa.aqilmusyaffa@gmail.com>
2024-05-01 16:43:07 +0300

    first commit

---
+++ Assets/Scripts/Human - Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HumanHealth : MonoBehaviour
+{
+    [SerializeField] private int healthPoints;
+
+    public void DecreaseHealth()
+    {
+        healthPoints--;
+    }
+
+    public int GetHealthPoints()
+    {
+        return healthPoints;
+    }
+
+}
